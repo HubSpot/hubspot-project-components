4b7acd64af18d779c200cf5186ff653450b8ffe1
Heather Weaver <heather@hweaver.com>
2026-04-16 10:21:41 -0400

    Add remaining files

--- 2026.03/private-app-get-started-template/src/app/cards/get-started-app-card.tsx
+++ 2026.03/private-app-get-started-template/src/app/cards/get-started-app-card.tsx
@@ -1,23 +1,24 @@
-import React from "react";
-import { Text, Link, List, EmptyState } from "@hubspot/ui-extensions";
-import { hubspot } from "@hubspot/ui-extensions";
+import { Text, Link, List, EmptyState } from '@hubspot/ui-extensions';
+import { hubspot } from '@hubspot/ui-extensions';
 
 hubspot.extend<'crm.record.tab'>(() => <Extension />);
 
 const Extension = () => {
-
-  const appCardDocsLink = 'https://developers.hubspot.com/docs/apps/developer-platform/add-features/ui-extensibility/app-cards/overview';
+  const appCardDocsLink =
+    'https://developers.hubspot.com/docs/apps/developer-platform/add-features/ui-extensibility/app-cards/overview';
 
   return (
     <>
       <EmptyState
         title="Build your app card here!"
         layout="vertical"
-        imageName='building'
+        imageName="building"
       >
         <Text>
-          Add a layer of UI customization to your app by including app cards that can display data, allow users to perform actions, and more.
-          Visit the <Link href={appCardDocsLink}>app card documentation</Link> for more info, or check out the following links to get inspired:
+          Add a layer of UI customization to your app by including app cards
+          that can display data, allow users to perform actions, and more. Visit
+          the <Link href={appCardDocsLink}>app card documentation</Link> for
+          more info, or check out the following links to get inspired:
         </Text>
         <List variant="unordered-styled">
           <Link href="https://developers.hubspot.com/docs/platform/ui-components">
