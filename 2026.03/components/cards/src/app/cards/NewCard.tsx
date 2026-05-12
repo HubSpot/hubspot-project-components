e292896cfdb5c944a3945dd31594e7a8ebefb311
jedeen-hs <jedeen+github@hubspot.com>
2026-05-12 09:44:57 -0500

    feat(2026.03): Improve types for UI Extensions templates

--- 2026.03/components/cards/src/app/cards/NewCard.tsx
+++ 2026.03/components/cards/src/app/cards/NewCard.tsx
@@ -1,15 +1,26 @@
-import { EmptyState, Link, Text } from '@hubspot/ui-extensions';
+import {
+  CrmContext,
+  EmptyState,
+  ExtensionPointApiActions,
+  Link,
+  Text,
+} from '@hubspot/ui-extensions';
 import { hubspot } from '@hubspot/ui-extensions';
 
-hubspot.extend<'crm.record.tab'>(({ context }) => (
-  <Extension context={context} />
+interface CrmExtensionProps {
+  context: CrmContext;
+  actions: ExtensionPointApiActions<'crm.record.tab'>;
+}
+
+hubspot.extend<'crm.record.tab'>(({ context, actions }: CrmExtensionProps) => (
+  <CrmExtension context={context} actions={actions} />
 ));
 
-const Extension = ({ context }) => {
+const CrmExtension = ({ context, actions }: CrmExtensionProps) => {
   const appCardDocsLink =
     'https://developers.hubspot.com/docs/apps/developer-platform/add-features/ui-extensibility/app-cards/overview';
 
-  console.log({ context });
+  console.log({ context, actions });
 
   return (
     <>
